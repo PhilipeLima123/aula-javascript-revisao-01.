5a8c5402d4bdce6a2bb622625c549b0a41f39663
Philipe Lima <philipelima803@gmail.com>
2021-09-22 18:01:01 -0300

    atv

---
+++ atv1.js
@@ -0,0 +1,9 @@
+let n1  = 0
+
+if (n1<0) {
+    console.log("O valor do número é negativo")
+} else if (n1>= 0) {
+    console.log("O valor Número é um Número positivo")
+} {
+
+}
---
+++ atv2.js
@@ -0,0 +1,16 @@
+function numeros (){
+
+    let a = 6
+    let b = 9
+    let c = 10
+
+    let soma = a + c
+
+    if ((a + c)> b) {
+        console.log(a + "+" + c + "=" + soma)
+    }else{
+        console.log(a + "+" + c + ">=" + b)
+    }
+
+}
+numeros()
